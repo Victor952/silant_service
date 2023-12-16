eeb70dbf7a0693513305e34b0246f7079d00f2d6
Victor952 <98576613+Victor952@users.noreply.github.com>
2023-12-16 18:37:44 +0300

    third commit

--- src/silant_service/urls.py
+++ src/silant_service/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from .views import home
+from silant.views import home_view
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
